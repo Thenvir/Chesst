c434240b5f735ac693d0038b0f8343484093c8c1
Thenvir <thenvira@gmail.com>
2020-08-29 21:41:38 -0400

    Finish rest of styling

--- app.js
+++ app.js
@@ -9,9 +9,43 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+// Local storage for now
+let games = [];
+function Game(title, link) {
+    this.title = title;
+    this.link = link;
+};
+var game1 = new Game("Me vs Adrian, Chigorin", "https://lichess.org/YlZfgDfs");
+games.push(game1);
 
+
+// Connect to mongo 
+
+
+// Render index page
 app.get('/', function(req, res){
-    res.render('index');
+    res.render('index', {games: games});
+});
+
+// Post Request
+app.post('/', function(req,res){
+
+    // Get user input & make a game object
+    const title = req.body.title;
+    let link = req.body.link;
+
+    // Validate inputs if empty / doesn't include http
+    if(title.length === 0 || link.length === 0){
+        return res.redirect('/');
+    }
+    if(!link.includes('https://')){
+        link = 'https://' + link; 
+    }
+    const userGame = new Game(title, link);
+
+    // Add to game array and render the home page
+    games.push(userGame);
+    return res.redirect('/');
 });
 
 app.listen(PORT, function(){
