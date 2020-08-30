a14b914a7f4bde3b7a882e4fa2fc1d8b94b58522
Thenvir <thenvira@gmail.com>
2020-08-29 23:30:30 -0400

    Configure mongoDB Locally for insert+delete

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const PORT = process.env.PORT || 3000;
 const express = require('express');
 const bodyParser = require('body-parser');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,20 +11,54 @@ app.set('view engine', 'ejs');
 
 // Local storage for now
 let games = [];
-function Game(title, link) {
-    this.title = title;
-    this.link = link;
-};
-var game1 = new Game("Me vs Adrian, Chigorin", "https://lichess.org/YlZfgDfs");
-games.push(game1);
+// function Game(title, link) {
+//     this.title = title;
+//     this.link = link;
+// };
+// var game1 = new Game("Me vs Adrian, Chigorin", "https://lichess.org/YlZfgDfs");
+// games.push(game1);
 
 
 // Connect to mongo 
+mongoose.connect("mongodb://localhost:27017/chesstDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+// Create Schema
+const gameSchema = {
+    title: String,
+    link: String,
+}
+
+// Create Model based on Schema
+const Game = mongoose.model("Game", gameSchema);
+
+// Default game
+const game1 = new Game({
+    title: "Me vs Adrian, Chigorin", 
+    link: "https://lichess.org/YlZfgDfs",
+});
+
+// Default games
+const defaultGames = [game1];
 
 
 // Render index page
 app.get('/', function(req, res){
-    res.render('index', {games: games});
+    // Get games from DB
+    Game.find({}, function(err, foundGames){
+        if(foundGames.length === 0){
+            // Insert default if DB is empty - else render games
+            Game.insertMany(defaultGames, function(err2){
+                if(err2){
+                    console.log(err2);
+                } else {
+                    console.log("Successfully saved default items to db");
+                }
+            });
+            res.redirect('/');
+        } else {
+            res.render('index', {games: foundGames});
+        }
+    });
 });
 
 // Post Request
@@ -41,11 +75,29 @@ app.post('/', function(req,res){
     if(!link.includes('https://')){
         link = 'https://' + link; 
     }
-    const userGame = new Game(title, link);
 
     // Add to game array and render the home page
-    games.push(userGame);
+    const userGame = new Game({
+        title: title, 
+        link: link,
+    });
+    userGame.save();
     return res.redirect('/');
+});
+
+app.post('/delete', function(req, res){
+    // Get game to delete by link
+    const gameToDelete = req.body.removeButton;
+    // Delete from DB and redirect to home 
+    Game.deleteOne({'link': gameToDelete}, function(err){
+        if(!err){
+            console.log("Successfully deleted item!");
+        }
+        else{
+            console.log(err);
+        }
+        res.redirect('/');
+    });
 });
 
 app.listen(PORT, function(){
